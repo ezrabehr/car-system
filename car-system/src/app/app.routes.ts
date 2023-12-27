27fa979eea7bf664cdd07d15c561bd14501e9c6c
yoda-gell <hvuskv6@gmail.com>
2023-12-28 00:49:17 +0200

    add bootstarp nav bar

--- car-system/src/app/app.routes.ts
+++ car-system/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    // { path: 'home', component: home },
+];
--- car-system/src/app/login/login.component.ts
+++ car-system/src/app/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.scss',
+    imports: [NavbarComponent]
 })
 export class LoginComponent {
 
