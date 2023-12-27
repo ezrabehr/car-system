5d9b3cf18bd69f11dced2c94a49c72398a2ac0bb
yoda-gell <hvuskv6@gmail.com>
2023-12-28 00:09:24 +0200

    finish main page

--- car-system/src/app/app.component.ts
+++ car-system/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from "./register/register.component";
+import { MainPageComponent } from "./main-page/main-page.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, RegisterComponent, MainPageComponent,NgbModule,]
 })
 export class AppComponent {
   title = 'car-system';
+
+ 
 }
